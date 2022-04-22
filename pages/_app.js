4ad0dff3e784d03d165d4fa9c0c43e67716c1131
Yuji <yujiricardo@gmail.com>
2022-04-22 17:15:21 -0300

    add: icons, layout

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,21 @@ import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+
+const drawerWidth = 240;
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -21,7 +36,47 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+<Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+              <ListItem button key="Livros">
+                <ListItemIcon>
+                <MenuBookIcon></MenuBookIcon>
+                </ListItemIcon>
+                <ListItemText primary="Livros" />
+              </ListItem>
+              <ListItem button key="Cadastro de livros">
+                <ListItemIcon>
+                <ModeEditOutlineIcon></ModeEditOutlineIcon>
+                </ListItemIcon>
+                <ListItemText primary="Cadastro de livros" />
+              </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         <Component {...pageProps} />
+
+      </Box>
+    </Box>
       </ThemeProvider>
     </CacheProvider>
   );
